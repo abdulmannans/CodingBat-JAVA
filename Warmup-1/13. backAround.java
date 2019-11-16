2f9847a4830b6454eaa4e7b88f9d70dabe206109
abdulmannans <samannan1999@gmail.com>
2019-11-16 06:44:51 -0800

    first commit

---
+++ Warmup-1/13. backAround.java
@@ -0,0 +1,4 @@
+public String backAround(String str) {
+  String back= str.substring(str.length()-1,str.length());
+  return back+str+back;
+}
---
+++ Warmup-1/8. posNeg.java
@@ -0,0 +1,9 @@
+public boolean posNeg(int a, int b, boolean negative) {
+  if ((a<0 && b>0 && !negative) || (a>0 && b<0 && !negative) || (a<0 && b<0 && negative)){
+  
+    return true;
+  }
+  else{
+    return false;
+  }
+}
